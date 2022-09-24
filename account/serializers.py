a908b1e80393a8e5431551abf9d0e99d380eec1f
Rechidesigns <alerechiordu@gmail.com>
2022-09-24 11:58:30 +0100

    update

--- account/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-class LogoutSerializer(serializers.Serializer):
-    refresh_token = serializers.CharField(max_length=700)
-    
-    
-class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField(max_length=700)
-    password = serializers.CharField(max_length=700)
--- retailapp/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Product, Cart
-
-admin.site.register([Product, Cart])
