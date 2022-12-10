2116084136edf383e95eab7fee7d162be1288b70
Alerechi Ordu <alerechiordu@gmail.com>
2022-12-10 01:13:26 +0100

    Update settings.py

--- retailshop/settings.py
+++ retailshop/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['tm30inventoryapp.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
